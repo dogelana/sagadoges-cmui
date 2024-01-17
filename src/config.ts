f2d965065804dd414862d46ee3120ac7e8a50278
dogelana <team@dogelana.com>
2024-01-17 03:10:27 -0500

    fix

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export const rpcHost =
 
 export const candyMachineId = new PublicKey(
   process.env.NEXT_PUBLIC_CANDY_MACHINE_ID ||
-    "3NynmKUJyZaH5ZPekXgpJavJo6ZL8yCSpCaaXnzyKREa"
+    "FVr5oQFUzxTEtttFPLVpncHxZh8f7xwxKx9yA7ANZgp5"
 );
 export const defaultGuardGroup =
   process.env.NEXT_PUBLIC_DEFAULT_GUARD_GROUP || undefined; // undefined means default
